90d36b76f313bdbc5c7dd66d3889dd4c6917e453
Daniel Nav <cdng9202@gmail.com>
2024-07-17 13:16:29 +0200

    add lighting

--- src/routes/scene.ts
+++ src/routes/scene.ts
@@ -13,18 +13,33 @@ export function createScene(gameWindow: HTMLElement) {
 
 	let meshes: THREE.Mesh[][] = [];
 
+	const setupLights = () => {
+		const lights = [
+			new THREE.AmbientLight(0xffffff, 0.2),
+			new THREE.DirectionalLight(0xffffff, 0.3),
+			new THREE.DirectionalLight(0xffffff, 0.3),
+			new THREE.DirectionalLight(0xffffff, 0.3)
+		];
+
+		lights[1].position.set(0, 1, 0);
+		lights[2].position.set(1, 1, 0);
+		lights[3].position.set(0, 1, 1);
+		scene.add(...lights);
+	};
+
 	const initialize = (city: cityType) => {
 		scene.clear();
 		meshes = city.data.map((dataRow) =>
 			dataRow.map(({ x, y }) => {
 				const geometry = new THREE.BoxGeometry(1, 1, 1);
-				const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+				const material = new THREE.MeshLambertMaterial({ color: 0x009900 });
 				const mesh = new THREE.Mesh(geometry, material);
 				mesh.position.set(x, 0, y);
 				scene.add(mesh);
 				return mesh;
 			})
 		);
+		setupLights();
 	};
 
 	return { meshes, scene, renderer, initialize };
